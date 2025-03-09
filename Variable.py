757cfc0aeb7d958e651f94e884ab170ec0dd997d
Sandeep Kumar <Ksundip750@gmail.com>
2025-03-10 00:01:42 +0530

    Add Variable.py with examples of variable declaration, casting, and usage

---
+++ Variable.py
@@ -0,0 +1,83 @@
+# example one
+x = 7
+y = 10
+
+print(x)
+print(y)
+
+# example 2
+name = "Sandeep Kumar"
+print(name)
+
+
+# Casting
+num1 = str(3)
+num2 = int(4)
+num3 = float(5)
+
+print(num1)
+print(num2)
+print(num3)
+
+# type()
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+# string varibale can be declared either by using single or double qoutes
+myName = "Sandeep"
+myAdd = 'Kishanganj'
+print(myName)
+print(myAdd)
+
+# Case-Sensitive
+a = 4
+A = "SanKonda"
+print(a)
+print(A)
+
+
+
+
+# Legal and Illegal Variables
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+# Illegal Variable
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+
+# multible varibles
+x, y, z = "Sandeep", '24', 'Kishanganj'
+print(x)
+print(y)
+print(z)
+
+# one value to multiple variables
+x = y = z = "Coder"
+print(x)
+print(y)
+print(z)
+
+# unpack a Collection
+Arrays = ['Sandeep', 'Age 24', 'Kishanganj']
+x , y, z = Arrays
+print(x)
+print(y)
+print(z)
+
+# in Python Constant is Global
