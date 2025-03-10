73405f1c92477b4e389df9c9535f43e76902e64a
Sandeep Kumar <Ksundip750@gmail.com>
2025-03-10 23:13:57 +0530

    Add DataTypes.py with examples of various data type declarations and their usage

---
+++ DataTypes.py
@@ -0,0 +1,49 @@
+x = 'Sandeep Kumar'
+print(type(x))
+
+x = 20
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = 1j
+print(type(x))
+
+x = ['Sandeep Kumar', 'Age 24', 'Kishanganj']
+print(type(x))
+
+x = ('Sandeep Kumar', 'Age 24', 'Kishanganj')
+print(type(x))
+
+x = range(75)
+print(type(x))
+
+
+x = {'name': "Sandeep", 'age': 24, 'Address': 'Kishaganj'}
+print(type(x))
+
+x = {'Sandeep', 'Web designer', 'frontend'}
+print(type(x))
+
+x = frozenset({"Sandeep", "Age 24", 'Frontend'})
+print(type(x))
+
+
+x = True
+print(type(x))
+
+
+x = b"Hello world"
+print(type(x))
+
+x = bytearray(bytes(10))
+print(type(x))
+
+
+x = memoryview(bytes(4))
+print(type(x))
+
+
+x = None
+print(type(x))
